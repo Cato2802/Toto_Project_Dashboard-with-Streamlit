487f21407f95f781a6c89c2223052ecd3bdf967d
Cato_Dang <137042784+Cato2802@users.noreply.github.com>
2024-04-01 10:30:01 +0700

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import plost
 
 st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 
@@ -8,31 +7,29 @@ with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 st.sidebar.header('Dashboard')
-plot_data = st.sidebar.multiselect('Select data', ['open', 'close'], ['open', 'close'])
-st.sidebar.subheader('Heat map parameter')
-time_hist_color = st.sidebar.selectbox('Color by', ('open', 'close'))
+plot_data = st.sidebar.multiselect('Chọn dữ liệu', ['Giá mở cửa', 'Giá đóng cửa'], ['Giá mở cửa', 'Giá đóng cửa'])
+st.sidebar.subheader('Thiết lập màu cho biểu đồ nhiệt độ')
+time_hist_color = st.sidebar.selectbox('Màu theo', ('Giá mở cửa', 'Giá đóng cửa'))
 
-st.sidebar.subheader('Line chart parameters')
-plot_height = st.sidebar.slider('Specify plot height', 200, 500, 250)
+st.sidebar.subheader('Thiết lập biểu đồ đường')
+plot_height = st.sidebar.slider('Chọn chiều cao của biểu đồ', 200, 500, 250)
 
-# Row A
-st.markdown('### Metrics')
+# Hàng A
+st.markdown('### Các Chỉ Số')
 col1, col2, col3 = st.columns(3)
-col1.metric("USD/VND", "23,667 VND", "1.2 VND")
-col2.metric("Change", "0.2%", "-8%")
-col3.metric("Volume", "1 million", "4 million")
+col1.metric("Giá FPT", "60,000 VND", "1,000 VND")
+col2.metric("Thay đổi", "0.5%", "-2%")
+col3.metric("Khối lượng", "2 triệu", "3 triệu")
 
-# Row B
-usd_vnd = pd.read_csv('https://raw.githubusercontent.com/Cato2802/Toto_dashboard/main/D%E1%BB%AF%20li%E1%BB%87u%20L%E1%BB%8Bch%20s%E1%BB%AD%20USD_VND%20(1).csv', parse_dates=['date'])
+# Hàng B
+fpt_stock = pd.read_csv('https://raw.githubusercontent.com/Cato2802/Toto_dashboard/main/D%E1%BB%AF%20li%E1%BB%87u%20ch%E1%BB%A9ng%20kho%C3%A1n%20FPT.csv', parse_dates=['Ngày'])
 
 c1, c2 = st.columns((7,3))
 with c1:
-    st.markdown('### Line chart for Open and Close/month')
-    st.line_chart(usd_vnd.set_index('date')[['open', 'close']], height=plot_height)
+    st.markdown('### Biểu đồ đường cho Giá mở cửa và Giá đóng cửa/tháng')
+    st.line_chart(fpt_stock.set_index('Ngày')[['Giá mở cửa', 'Giá đóng cửa']], height=plot_height)
 
-# Row C
-st.markdown('### Line chart/Day')
-st.line_chart(usd_vnd, x='date', y=plot_data, height=plot_height)
+# Hàng C
+st.markdown('### Biểu đồ đường/ngày')
+st.line_chart(fpt_stock, x='Ngày', y=plot_data, height=plot_height)
 
-
-###If you want to run the Streamlit dictionary: Open cmd, Navigate to the code file directory using 'cd', Enter the command 'streamlit run Untitled-1.py'.
